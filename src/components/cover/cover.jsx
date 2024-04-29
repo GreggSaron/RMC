77046f2cec1f7f802228f89a3c1dc748a0bbd48c
Elena Medvedeva <elena.belyaeva2003@mail.ru>
2024-04-29 18:43:26 +0300

    second try

---
+++ src/components/cover/cover.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Cover = () => {
+  return (
+    <div>
+      <p>Architecture design</p>
+      <p>
+        Professional <br />
+        Interior Design
+      </p>
+      <p>
+        Design for now with the awareness of yesterday and unknown tomorrow.
+        Because small details make the perfect design. Helping you choose
+        between good and best.
+      </p>
+    </div>
+  );
+};
+
+export default Cover;
+export { Cover };
