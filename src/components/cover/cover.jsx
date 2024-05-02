c527214473629d1fe4b1476e1ee10c2da86bb1ba
GreggSaron <gregory2002av@gmail.com>
2024-05-02 16:13:42 +0300

    cover 1st

--- src/components/cover/cover.jsx
+++ src/components/cover/cover.jsx
@@ -3,14 +3,33 @@ import React from "react";
 const Cover = () => {
   return (
     <div style={{
-      height: '100vh'
+      height: '100vh',
+      width: '50vw',
+      marginLeft: '100px',
+      marginRight: 'auto',
+      marginTop: '50px'
     }}>
-      <p>Architecture design</p>
-      <p>
+      <p style={{
+          fontFamily: 'Oswald',
+          color: '#FFC978',
+          fontSize: '20px',
+          margin: '0px'
+          }}>
+        Architecture design
+      </p>
+      <p style={{
+          fontFamily: 'Oswald',
+          fontSize: '84px',
+          margin: '0px'
+          }}>
         Professional <br />
         Interior Design
       </p>
-      <p>
+      <p style={{
+          fontFamily: 'Roboto',
+          fontSize: '16px',
+          margin: '0px'
+          }}>
         Design for now with the awareness of yesterday and unknown tomorrow.
         Because small details make the perfect design. Helping you choose
         between good and best.
